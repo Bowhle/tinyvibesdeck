dfbbb0802c2898d9882b63b4d577876400bc9f7a
Sammy Mkwananzi <SammyM@tregerbrands.local>
2025-10-03 17:29:10 +0200

    Trying out a simple gif for the Hero

--- vintage-music-festival/landing-page/src/components/Hero.js
+++ vintage-music-festival/landing-page/src/components/Hero.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Hero.css';
-import poster from '../assets/Vintage_Music_Festival_Digital.jpg';
+import poster from '../assets/Vintage_Music_Festival_Digital.gif';
+
+/**
+ * Returns a hero section with an image of the festival poster.
+ * 
+ * @returns {JSX.Element} A JSX element representing the hero section.
+ */
 
 function Hero() {
   return (
